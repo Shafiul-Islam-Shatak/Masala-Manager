2a386918383f2490cc957eba691f80b517bf62d9
Shatak-the-imposter <helloshatak@gmail.com>
2024-05-11 22:23:19 +0600

    can post a food to mongodb

--- src/Pages/AddFood.jsx
+++ src/Pages/AddFood.jsx
@@ -1,14 +1,54 @@
 import { useContext } from "react";
 import { AuthContext } from "../Provider/AuthProvider";
+import Swal from 'sweetalert2'
+
 
 const AddFood = () => {
-    const { user } = useContext(AuthContext)
+    const { user } = useContext(AuthContext);
+    const hanldeAddFood = e => {
+        e.preventDefault();
+        const form = e.target;
+        const food_name = form.food_name.value;
+        const category = form.category.value;
+        const image = form.food_photo.value;
+        const price = form.price.value;
+        const quantity = form.quantity.value;
+        const origin = form.origin.value;
+        const userName = form.username.value;
+        const userEmail = form.email.value;
+        const description = form.description.value;
+
+        const food = { food_name, category, image, price, quantity,origin,  userName, userEmail, description };
+        console.log(food);
+
+        // post data to backend
+        fetch('http://localhost:5000/all-foods', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(food)
+
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.insertedId) {
+                    Swal.fire({
+                        title: "Congratulation !!",
+                        text: "Succesfully Craft Added",
+                        icon: "success"
+                    });
+                    form.reset();
+                }
+            })
+    }
     return (
         <div>
             <section className="w-3/4 mx-auto ">
                 <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">Add a Delicious Food</h2>
 
-                <form>
+                <form  onSubmit={hanldeAddFood}>
                     <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
                         {/* row 1 */}
                         <div>
@@ -49,7 +89,7 @@ const AddFood = () => {
 
                         {/* Row 4 */}
                         <div>
-                            <label htmlFor="username" className="text-gray-700 dark:text-gray-200 font-semibold">Added By</label>
+                            <label  className="text-gray-700 dark:text-gray-200 font-semibold">Added By</label>
                             <input value={user?.displayName} name="username" type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
                         </div>
 
