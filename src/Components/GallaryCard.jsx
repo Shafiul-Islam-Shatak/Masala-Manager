c1d44fa250396ebbd98fad797c7ee5bf52b99f37
Shatak-the-imposter <helloshatak@gmail.com>
2024-05-12 22:54:47 +0600

    gallery page done

--- src/Components/GallaryCard.jsx
+++ src/Components/GallaryCard.jsx
@@ -1,7 +1,19 @@
-const GallaryCard = () => {
+const GallaryCard = ({feedbackss}) => {
+    const {reviwer_name, review_iamge, feedback} = feedbackss;
     return (
         <div>
-            
+            <div className="relative group flex flex-col items-center justify-center w-full max-w-sm mx-auto">
+                    <div className="relative w-full h-64 overflow-hidden rounded-lg shadow-md group-hover:opacity-100">
+                        <img className="w-80 md:w-96 rounded-lg object-cover" src={review_iamge} alt="Nike Revolt" />
+                        <div className="absolute inset-0 flex items-center justify-center opacity-0 bg-black bg-opacity-50 group-hover:opacity-100 transition-opacity duration-300">
+                            <div className="px-5">
+                            <h1 className="text-white text-2xl font-semibold">{reviwer_name}</h1>
+                            <h1 className="text-white font-semibold mt-5">{feedback}</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
         </div>
     );
 };
--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -1,16 +1,19 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../Provider/AuthProvider";
 import toast from "react-hot-toast";
+import axios from 'axios'
+import GallaryCard from "../Components/GallaryCard";
 
 const Gallery = () => {
+    const [feedbacks, setFeedbacks] = useState([])
     const { user } = useContext(AuthContext)
     const hanldeAddFeedBack = e => {
         e.preventDefault();
         const form = e.target;
-        const reviwer_name = user?.displayName
-        const reviwer_email = user?.email
+        const reviwer_name = user?.displayName;
+        const review_iamge = form.review_iamge.value;
         const feedback = form.feedback.value;
-        const review = { reviwer_name, reviwer_email, feedback }
+        const review = { reviwer_name, review_iamge, feedback }
         console.log(review);
 
         // post data to backend
@@ -28,24 +31,50 @@ const Gallery = () => {
                 if (data.insertedId) {
                     toast.success('Feedback Done')
                     form.reset();
+                    const newFeedback = [...feedbacks, review]
+                    setFeedbacks(newFeedback)
                 }
             })
     }
+
+    useEffect(() => {
+        const getData = async () => {
+            const { data } = await axios(`${import.meta.env.VITE_API_URL}/feedbacks`)
+            setFeedbacks(data)
+        }
+        getData()
+
+    }, [])
+
+    console.log(feedbacks);
     return (
         <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://img.freepik.com/free-photo/pre-prepared-food-showcasing-ready-eat-delicious-meals-go_23-2151431678.jpg?t=st=1715510852~exp=1715514452~hmac=43587f936343406381eac567bf4e46602240dc08774108600d4b7d07237a79a7&w=900)' }}>
             <div className="hero-overlay bg-opacity-60" > </div>
             <div className="w-3/4">
-                {/* card */}
-                <div className="relative group flex flex-col items-center justify-center w-full max-w-sm mx-auto">
-                    <div className="relative w-full h-64 overflow-hidden rounded-lg shadow-md group-hover:opacity-100">
-                        <img className="w-full h-full object-cover" src="https://images.unsplash.com/photo-1521903062400-b80f2cb8cb9d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80" alt="Nike Revolt" />
-                        <div className="absolute inset-0 flex items-center justify-center opacity-0 bg-black bg-opacity-50 group-hover:opacity-100 transition-opacity duration-300">
-                            <p className="text-white font-semibold">Nike Revolt</p>
+                <div className="bg-transparent">
+                    <div className="hero-content flex-col lg:flex-row">
+                        <img  src="https://img.freepik.com/free-photo/full-shot-smiley-woman-with-smartphone_23-2149394418.jpg?t=st=1715531639~exp=1715535239~hmac=3a09e972d102a14ea20b0f0a4ce1dd01cbd2d1a59fa40fd2b1d6194592868ca7&w=900" className="w-80 md:w-96 rounded-lg shadow-2xl" />
+                        <div>
+                            <h1 className="text-5xl text-white font-bold">Our Honorable Clients Feedback</h1>
+                            <p className="py-6 text-white">Dive into the feedback from our esteemed clients, showcasing their valuable testimonials and insights. Discover how our products and services have made a positive impact on their experiences. Join us in celebrating their satisfaction and loyalty as we continue to strive for excellence.</p>
+                            <button onClick={() => document.getElementById('my_modal_5').showModal()} className="btn mt-10 hover:bg-orange-600 bg-orange-300 text-black w-1/2">Add Your Feedback</button>
                         </div>
                     </div>
                 </div>
+                <h2 className="text-white font-bold text-3xl md:text-5xl  text-center mb-10 mt-10 w-3/4 mx-auto">Take a Look about Ourself</h2>
 
-                <button onClick={() => document.getElementById('my_modal_5').showModal()} className="btn mt-10 hover:bg-orange-600 bg-orange-300 text-black w-full">Add</button>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+                    {
+                        feedbacks.map(feedback =>
+                            <GallaryCard
+                                key={feedback._id}
+                                feedbackss={feedback}
+                            ></GallaryCard>)
+                    }
+
+                </div>
+
+                
                 {/* Open the modal using document.getElementById('ID').showModal() method */}
                 <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
                     <div className="modal-box">
@@ -60,18 +89,18 @@ const Gallery = () => {
                                     </div>
 
                                     <div>
-                                        <label className="text-gray-700 dark:text-gray-200" >Email Address</label>
-                                        <input name="reviwer_email" defaultValue={user?.email} readOnly type="email" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                                        <label className="text-gray-700 dark:text-gray-200" >Image URL</label>
+                                        <input required name="review_iamge" type="url" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
                                     </div>
 
                                     <div className="w-full">
                                         <label className="text-gray-700 dark:text-gray-200">FeedBack</label>
-                                        <textarea name="feedback" type="text" className="block w-[300px] md:w-[415px] px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                                        <textarea required name="feedback" type="text" className="block w-[300px] md:w-[415px] px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
                                     </div>
                                 </div>
                                 <button onClick={() => document.getElementById('my_modal_5').close()} type="submit" className="mt-8 w-full btn block px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Save</button>
                             </form>
-                            
+
                         </section>
 
                     </div>
